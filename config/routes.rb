3fc74cfdaddb899de99e64ac0259e7f0c0d6b8a0
AngeManto98 <koutikaangemarie@gmail.com>
2021-01-06 10:47:40 +0000

    Added routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,38 @@
 Rails.application.routes.draw do
  
   devise_for :users
-  
+
   root to: "home#index"
+  
+  resources :users,  only: [:show,  :index]
+  resources :payments,  only: [:show,  :index]
+  resources :groups,  only: [:show,  :index]
+  resources :settings,  only: [:show,  :index]
+
+  namespace :admin do
+    resources :users do
+      collection do
+        post :confirm
+      end
+    end
+
+    resources :groups do
+      collection do
+        post :confirm
+      end
+    end
+
+    resources :payments do
+      collection do
+        post :confirm
+      end
+    end
+
+    resources :settings do
+      collection do
+        post :confirm
+      end
+    end
+  end
+
 end
