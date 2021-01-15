080ba562c01a6af6165ce94d7aeab1250ac51aa1
AngeManto98 <koutikaangemarie@gmail.com>
2021-01-15 12:04:49 +0000

    Added default value to group and setting in seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
-User.create(name: 'Admin1', first_name: 'Admin', telephone: 0000000, added_since: "2020-01-01", email: 'admin1@example.com', group_id: 1, admin: 1, setting_id: 1, password: '123456', password_confirmation: '123456')
+#User.create(name: 'Admin1', first_name: 'Admin', telephone: 0000000, added_since: "2020-01-01", email: 'admin1@example.com', group_id: 1, admin: 1, setting_id: 1, password: '123456', password_confirmation: '123456')
+Group.create(name: 'Group1', description: 'Le groupe 1');
+Setting.create(amount_due: 30000, devise: 'FCFA')
