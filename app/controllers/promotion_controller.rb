057f14ca8995ca180a7af50a129df211601e77a1
AngeManto98 <koutikaangemarie@gmail.com>
2021-01-17 08:45:47 +0000

    Added content to promotion_controller and promotion view.

--- app/controllers/promotion_controller.rb
+++ app/controllers/promotion_controller.rb
@@ -1,4 +1,17 @@
 class PromotionController < ApplicationController
+  before_action :authenticate_user!, except: %i[index]  
+  before_action :set_promotion, only: %i[index]
+
   def index
+    @user = User.all
+    @promotions = Promotion.all 
+  end
+
+  private 
+  def promotion_params
+    params.permit(:name, :description, :user_id)
+  end 
+  def set_promotion
+    @group = Group.find_by(params[:id])
   end
 end
