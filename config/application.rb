ffc8ee267900844e1666790a1ccfc995c9027788
AngeManto98 <koutikaangemarie@gmail.com>
2021-01-22 14:11:58 +0000

    Added comment i18n.

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module JCIApp
   class Application < Rails::Application
     config.load_defaults 5.2
   
-    config.i18n.default_locale = :fr
+    #config.i18n.default_locale = :fr
     config.exceptions_app = self.routes
     config.assets.initialize_on_precompile = false
   end
