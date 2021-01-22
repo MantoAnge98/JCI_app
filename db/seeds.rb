3cad6b9d36182db0d3df0e75519914ff60ea1eb7
AngeManto98 <koutikaangemarie@gmail.com>
2021-01-22 14:36:13 +0000

    Added new seed content.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-#User.create(name: 'Admin1', first_name: 'Admin', birthday: "1990-01-01", profession: "Boss", telephone: 0000000, added_since: "2020-01-01", email: 'admin1@example.com', group_id: 1, admin: 1, setting_id: 1, promotion_id: 1, password: '123456', password_confirmation: '123456')
-Group.create(name: 'Group1', description: 'Le groupe 1');
-Setting.create(amount_due: 30000, devise: 'FCFA');
-Promotion.create(name: "Promo 1", description: "La promo1", year_promotion: "2018-05-25")
+User.create(name: 'Intside', first_name: 'Intside', birthday: "2015-01-01", profession: "Admin", telephone: 0000000, added_since: "2020-06-01", email: 'admin@intside.com', group_id: 1, admin: 1, setting_id: 1, promotion_id: 1, password: 'Q1W2E3R4', password_confirmation: '123456')
+#Group.create(name: 'Group1', description: 'Le groupe 1');
+#Setting.create(amount_due: 30000, devise: 'FCFA');
+#Promotion.create(name: "Promo 1", description: "La promo1", year_promotion: "2018-05-25")
