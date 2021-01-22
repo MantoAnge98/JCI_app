f2df1b59c77f30a2b8575025bf857fc320becf14
AngeManto98 <koutikaangemarie@gmail.com>
2021-01-22 13:33:59 +0000

    Added error to routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
  
-  get 'errors/not_found'
-  get 'errors/internal_server_error'
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
+
+  #Devise user
   devise_for :users
 
+  #Routes acces
   root to: "home#index"
   
   #Json fromat routes
@@ -17,12 +20,14 @@ Rails.application.routes.draw do
     end
    end
 
+  #All ressources pages
   resources :users,       only: [:show]
   resources :payments,    only: [:index]
   resources :groups,      only: [:index]
   resources :settings,    only: [:index]
   resources :promotions,  only: [:index]
 
+  #All ressources admin pages
   namespace :admin do
     resources :users do
       collection do
